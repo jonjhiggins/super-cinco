62d1f3d879c1ab8ab5154cfe323f0e03b9e515c5
Jon Higgins <jon@jonhiggins.co.uk>
2017-04-14 16:14:38 +1000

    switch to using .env for environment variables

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express()
 const routes = require('./routes')
 const bodyParser = require('body-parser')
 const config = require('dotenv').config()
-
-console.log(config)
+const dbType = process.env.NODE_ENV.toUpperCase()
 
 app.superCinco = {
-  config: config.parsed
+  config: {
+    DB_HOST: config.parsed['DB_' + dbType + '_HOST'],
+    DB_USER: config.parsed['DB_' + dbType + '_USER'],
+    DB_PASS: config.parsed['DB_' + dbType + '_PASS'],
+    DB_SSL: config.parsed['DB_' + dbType + '_SSL'] === 'true',
+    DB_AUTH_SOURCE: config.parsed['DB_' + dbType + '_AUTH_SOURCE']
+  }
 }
 
 app.use(bodyParser.json()) // to support JSON-encoded bodies
--- super-cinco/app.js
+++ super-cinco/app.js
@@ -7,32 +7,27 @@ const successTheme = chalk.bold.green
 const monk = require('monk')
 
 /**
- * Prepare DB connection
- * @function prepareDb
- * @param {string} username db username
- * @param {string} password db password
- * @param {string} url db url
- * @param {object} options db connection options
- * @returns {object} monk/mongodb DB object
- */
-const prepareDb = function (username, password, url, options) {
-  console.log(username, password, url)
-  const dbUrl = username + ':' + encodeURIComponent(password) + '@' + url
-  const db = monk(dbUrl)
-  return db
-}
-
-/**
  * Add a song to the songs database
  * @function addSong
- * @param {object} dbConfig object containing database settings
+ * @param {object} config object containing database settings
  * @param {string} artist artist of song to add
  * @param {string} song title of song to add
  * @param {boolean} verbose should it log out errors etc
  * @returns {promise} resolved/rejected string
  */
-const addSongToDb = function (dbConfig, artist, song, verbose = false) {
-  return prepareDb(dbConfig['DB_USER'], dbConfig['DB_PASS'], dbConfig['DB_HOST'], dbConfig.options)
+const addSongToDb = function (config, artist, song, verbose = false) {
+
+  const dbConfig = {
+    username: config['DB_USER'],
+    password: config['DB_PASS'],
+    host: config['DB_HOST'],
+    options: {
+      ssl: config['DB_SSL'],
+      authSource: config['DB_AUTH_SOURCE']
+    }
+  }
+
+  return prepareDb(dbConfig)
           .then(addSong.bind(null, artist, song))
           .then(msg => {
             if (verbose) {
@@ -46,6 +41,24 @@ const addSongToDb = function (dbConfig, artist, song, verbose = false) {
             }
             throw err
           })
+}
+
+/**
+ * Prepare DB connection
+ * @function prepareDb
+ * @param {object} dbConfig object containing db details
+ * @param {string} dbConfig.username db username
+ * @param {string} dbConfig.password db password
+ * @param {string} dbConfig.host db host url
+ * @param {object} dbConfig.options db connection options
+ * @param {boolean} dbConfig.options.ssl db ssl on or off
+ * @param {string} dbConfig.options.authSource db to authenticate against
+ * @returns {object} monk/mongodb DB object
+ */
+const prepareDb = function ({username, password, host, options}) {
+  const dbUrl = username + ':' + encodeURIComponent(password) + '@' + host
+  const db = monk(dbUrl, options)
+  return db
 }
 
 module.exports = {
