0d247d33689d6b97e8e928a5ae82871dc71b3f3c
Jon Higgins <jon@jonhiggins.co.uk>
2017-04-15 19:05:34 +1000

    servers adds song to db (hardcoded song)

--- add-song/handler.js
+++ add-song/handler.js
@@ -1,16 +1,34 @@
-'use strict';
+const superCinco = require('../super-cinco/app')
+const config = require('dotenv').config({path: '../.env'})
+const buildConfig = require('../lib/build-config')
+console.log(config.parsed)
+const appConfig = buildConfig(config.parsed, 'DEV')
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.addSong = (event, context, callback) => {
+  // const artist1 = req.body['artist-1']
+  // const song1 = req.body['song-1']
+  //
+  const artist1 = 'artist-2'
+  const song1 = 'song-2'
+  superCinco.addSong(appConfig, artist1, song1)
+    .then(msg => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          msg: msg
+        })
+      }
 
-  callback(null, response);
+      callback(null, response)
+    })
+    .catch(err => {
+      const response = {
+        statusCode: 500,
+        body: JSON.stringify({
+          msg: err.name + ': ' + err.message
+        })
+      }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+      callback(null, response)
+    })
+}
